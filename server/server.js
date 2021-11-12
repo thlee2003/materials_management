a95d2657ec399a52de710b7f17cd29213c096d6f
Lee TaeHee <taehee0321@iwsolution.co.kr>
2021-11-12 16:43:46 +0900

    mariaDB

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const port = 5000;
+const mysql = require('mysql');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : 'jeff0520',
+    database : 'management',
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.post("/login", (req, res) => {
+    const userName = req.body.userName;
+    const userEmail = req.body.userEmail;
+    console.log(userName, userEmail);
+
+    let sendData;
+    if(userName && userEmail) {
+        const sqlQuery = 'SELECT * FROM login WHERE user_name = ? AND user_id = ?';
+        db.query(sqlQuery, [userName, userEmail], (err, results, fields) => {
+            if(!err && results<=0) {
+                console.log("로그인에 실패하였습니다.")
+                sendData = {
+                    data: 'false'
+                }
+                res.send(sendData);
+            } else {
+                console.log("로그인에 성공하였습니다.")
+                sendData = {
+                    data: 'true'
+                }
+                res.send(sendData);
+            }
+        })
+    }
+    // const sqlQuery = "INSERT INTO login (user_name,user_id) VALUES (?,?)";
+    // db.query(sqlQuery, [userName,userEmail], (err, result) => {
+    //     res.send('success!');
+    // });
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+ 
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+// import { response } from 'express';
+import React, { useState, useEffect } from 'react';
 import styles from './Login.module.css';
 
 const Login = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:5000/login", {
+  //     userName: name,
+  //     userEmail: email
+  //   }).then((response)=> {
+  //     console.log(response.data);
+  //   })
+  // }, []);
+
+
   const onsubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email);
-    window.location.replace('/Main');
+    e.preventDefault();  
+    axios.post("http://localhost:5000/login", {
+      userName: name,
+      userEmail: email
+    }).then((response) => {
+      if(response.data.data == 'true') {
+        window.location.replace('/Main');
+      } else {
+        alert("로그인에 실패하였습니다.")
+      }
+    })
   };
   return (
     <div>
       <form onSubmit={onsubmit} className={styles.login}>
-        <input className={styles.input} type="text" value={name} onChange={(e) => setName(e.target.value)} />
-        <input className={styles.input} type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input className={styles.input} placeholder="이름" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        <input className={styles.input} placeholder="이메일" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <button className={styles.button}>로그인</button>
       </form>
     </div>
