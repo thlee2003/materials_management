a47acfa9a21d10230b521db5f23055584439c878
ggw <ggw>
2021-12-09 14:44:41 +0900

    ui

--- src/pages/AS/AS.js
+++ src/pages/AS/AS.js
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './AS.module.css';
 
 import { HotTable } from '@handsontable/react';
 import Handsontable from 'handsontable';
+import { HyperFormula } from 'hyperformula';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
 
 import data from '../../data.json';
 
+let hot;
+const table = () => {
+  const column = ['코드', '제품명', '시리얼코드', '판매 방법', '수량', '날짜', '이름', '주소', '연락처', '수리비', '현황', '수리내역'];
+  const hyperformulaInstance = HyperFormula.buildEmpty();
+  const container = document.getElementById('container');
+  let hotData = [['', '', '', '', '', '', '', '', '', '', '', '수리내역']];
+
+  hot = new Handsontable(container, {
+    className: 'htCenter',
+    data: hotData,
+    colHeaders: column,
+    rowHeaders: true,
+    width: '100%',
+    height: '730',
+    licenseKey: 'non-commercial-and-evaluation',
+    stretchH: 'all',
+    columns: [
+      {},
+      {},
+      {},
+      {},
+      { type: 'numeric' },
+      {},
+      {},
+      {},
+      {},
+      {},
+      { type: 'dropdown', source: ['AS 접수', '수리 중', '수리 완료'] },
+      {},
+    ],
+  });
+
+  hot.updateSettings({
+    afterChange: function () {
+      console.log(hotData);
+    },
+  });
+};
+
 const AS = () => {
+  useEffect(() => {
+    table();
+  }, []);
   const column = ['코드', '제품명', '시리얼코드', '판매 방법', '수량', '날짜', '이름', '주소', '연락처', '수리비', '현황'];
   const links = [
     {
@@ -36,30 +79,7 @@ const AS = () => {
       <div className={styles.list}>
         <h1 className={styles.h1}>AS 목록</h1>
         <div className={styles.table}>
-          <HotTable
-            className="htCenter"
-            data={hotData}
-            colHeaders={column}
-            rowHeaders={true}
-            width="100%"
-            height="730"
-            licenseKey="non-commercial-and-evaluation"
-            stretchH="all"
-            // readOnly
-            columns={[
-              {},
-              {},
-              {},
-              {},
-              {},
-              { type: 'numeric', numericFormat: { pattern: ' 0,0' } },
-              {},
-              {},
-              {},
-              {},
-              { type: 'dropdown', source: ['주문 접수', '포장 중', '포장 완료', '배송 완료'] },
-            ]}
-          />
+          <div id="container"></div>
         </div>
       </div>
     </div>
